9821decec596dc6e6a0922a5b38bdac536c14fc7
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-04-10 15:16:24 +0200

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Image } from "react-bootstrap";
 
 export default function Basic() {
   return (
     <Container>
-        <h1>Basic Example</h1>
+        <h3>Scheda Migrazione in Cloud</h3>
+        <Image src="https://raw.githubusercontent.com/CodeKaito/maggioli-schedamigrazione/main/src/assets/maggioli-cloud.png?token=GHSAT0AAAAAACPKO4CWTQRNCYUCALQA2K2QZQWSBMQ" alt="maggioli-cloud" height={50} width={50} />
     </Container>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,17 @@
 import { Image } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 function MyNavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">
-            <Image src="../assets/maggioli-cloud.png" alt="maggioli-cloud" />
+        <Navbar.Brand href="/">
+            <Image src="https://raw.githubusercontent.com/CodeKaito/maggioli-schedamigrazione/main/src/assets/maggioli-cloud.png?token=GHSAT0AAAAAACPKO4CWTQRNCYUCALQA2K2QZQWSBMQ" alt="maggioli-cloud" height={50} width={50} />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Maggioli Cloud</Nav.Link>
+            <Nav.Link href="/">Maggioli Cloud</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
